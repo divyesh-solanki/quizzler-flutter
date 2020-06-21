5c8058f8f73647e846cf354aba98598d72ddd40c
divyesh <skdivyesh333@gmail.com>
2020-06-21 12:33:45 +0530

    created Question class it includes questionText and answer created a list of Question called questionList and access data through it

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -27,14 +28,23 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-  ];
-
-  List<bool> answers = [false, true, true];
   int questionNo = 0;
+
+  List<Question> questionList = [
+    Question(
+      q: 'You can lead a cow down stairs but not up stairs.',
+      a: false,
+    ),
+    Question(
+      q: 'Approximately one quarter of human bones are in the feet.',
+      a: true,
+    ),
+    Question(
+      q: 'A slug\'s blood is green.',
+      a: true,
+    ),
+  ];
+  
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -47,7 +57,8 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNo],
+                questionList[questionNo].questionText,
+                //questions[questionNo],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -71,7 +82,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswer = answers[questionNo];
+                bool correctAnswer = questionList[questionNo].answer;
                 if (correctAnswer == true) {
                   print('user is correct');
                   setState(
@@ -110,7 +121,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswer = answers[questionNo];
+                bool correctAnswer = questionList[questionNo].answer;
                 if (correctAnswer == false) {
                   setState(
                     () {
